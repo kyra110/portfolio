e96885a647c4772c57d8b9d1eaaebc191b8d657f
Michel <michel.alex.partenaire@gmail.com>
2024-04-11 19:25:28 +0200

    Ajout des premiers composants Header et Navifation

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+
+
+const Navigation = () => {
+  return (
+  <ul className="header__nav">
+    <li>accueil</li>
+    <li>à-propos</li>
+    <li>projets</li>
+  </ul>
+  );
+};
+
+export default Navigation;
