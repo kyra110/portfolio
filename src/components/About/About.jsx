ce4c8f6e5c488f93241d8cb21bbd7856d42ce51e
Michel <michel.alex.partenaire@gmail.com>
2024-04-14 23:20:08 +0200

    Refonte de la page about et ajustement du responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,30 +1,30 @@
-
 const About = () => {
   return (
     <section className="a-propos">
-      <h1>A-propos</h1>
-      <div className="video-presentation">
-      <h2>Présentation en vidéo !</h2>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/j0zuYReZyhE"
-        title="Vidéo de présentation"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-      </div>
-      <div className="video-code">
-      <h2>Voici une vidéo ou je code</h2>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/xep1qYUswOQ"
-        title="Vidéo de présentation"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-      </div>
+      <h1>Un peu plus ?</h1>
+      <p>
+        Je vis dans le sud de l&apos;Occitanie et j&apos;aime sortir en montagne. De temps
+        en temps, je fais une petite randonnée pour prendre l&apos;air après une
+        semaine de travail.
+      </p>
+      <p>
+        Pendant une période hors saison, j&apos;ai réalisé que je voulais un travail
+        un peu plus stable. Je me suis donc tourné vers le web ! Chose que je ne
+        regrette pas du tout.
+      </p>
+      <p>
+        J&apos;ai également le projet de devenir freelance pour proposer mes services
+        d&apos;intégrateur web. J&apos;ai une chaîne YouTube où je partage mon parcours
+        avec tous et où j&apos;essaie de donner un maximum de conseils pour réussir
+        notre formation.
+      </p>
+      <p>
+        Actuellement, je recherche une alternance en PHP/Symfony pour compléter
+        mon arsenal de compétences. Si vous êtes une entreprise, n&apos;hésitez donc
+        pas à me contacter grâce au formulaire de contact en page d&apos;accueil.
+      </p>
+
+      <h2>Je vous souhaite une bonne visite.</h2>
     </section>
   );
 };
